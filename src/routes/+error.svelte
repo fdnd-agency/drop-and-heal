0b9ee6a76b10aca9764fadf85bb273a78b9fd187
Latoya <76912011+latoyaln@users.noreply.github.com>
2025-02-07 10:14:07 +0100

    feat: error page #62

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import BlurgradPink from '$lib/blurgrad-pink.svelte';
+    import HeaderIntro from '$lib/HeaderIntro.svelte';
+    import { page } from '$app/stores';
+</script>
+
+<main class="intro-main">
+    <HeaderIntro headerText_l1="Whoops! Something went wrong" headerText_l2="" progressValue={0} />
+    <h2>{$page.status}: {$page.error?.message} </h2>
+    <BlurgradPink pageStyle=intro_algemeen/>
+</main>
+
+<style>
+h2 {
+    font-family: Poppins;
+    margin: 0;
+    font-size: 2em;
+    max-width: none;
+  }
+
+</style>
--- src/routes/introductie-rouwtaken/+page.server.js
+++ src/routes/introductie-rouwtaken/+page.server.js
@@ -4,7 +4,6 @@ export async function load() {
   const url = 'https://fdnd-agency.directus.app/items/dropandheal_task';
 
   const response = await fetchJson(url);
-  console.log('API Response:', response);
 
   const task = response.data.map(task => ({
     ...task,
