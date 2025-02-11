afc0eff13dec9c72e3b63a0f3cccdc6a19f8295a
Latoya <76912011+latoyaln@users.noreply.github.com>
2025-02-11 10:46:03 +0100

    feat: gradient-background #62

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,21 +1,23 @@
 <script>
-    import BlurgradPink from '$lib/blurgrad-pink.svelte';
+    import GradientBackground from '$lib/molecules/gradient-background.svelte';
     import HeaderIntro from '$lib/HeaderIntro.svelte';
     import { page } from '$app/stores';
 </script>
 
-<main class="intro-main">
+<main class="error-page">
+    <GradientBackground/>
     <HeaderIntro headerText_l1="Whoops! Something went wrong" headerText_l2="" progressValue={0} />
     <h2>{$page.status}: {$page.error?.message} </h2>
-    <BlurgradPink pageStyle=intro_algemeen/>
+
 </main>
 
 <style>
-h2 {
-    font-family: Poppins;
-    margin: 0;
-    font-size: 2em;
-    max-width: none;
-  }
+    
+    h2 {
+        font-family: Poppins;
+        margin: 0;
+        font-size: 2em;
+        max-width: none;
+    }
 
 </style>
