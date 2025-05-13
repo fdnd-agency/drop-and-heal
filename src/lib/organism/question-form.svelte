86771635e1c16d6ac165630d28e7e4938f9be77b
zainablfz <144009548+zainablfz@users.noreply.github.com>
2025-05-13 19:24:00 +0200

    style: #202 changed html and styling of the form

--- src/lib/organism/question-form.svelte
+++ src/lib/organism/question-form.svelte
@@ -2,28 +2,97 @@
   export let surveyData = [];
 </script>
 
-{#each surveyData as question}
-  <fieldset>
-    <legend>{question.legend}</legend>
-    {#each question.options as option}
-      <div>
-        <label>
-          <input type="radio" name={question.name} value={option.value} />
-          {option.labelText}
-        </label>
-      </div>
-    {/each}
-  </fieldset>
-{/each}
+<form method="POST" action="?/submit">
+  {#each surveyData as question}
+    <fieldset>
+      <legend>{question.legend}</legend>
+      {#each question.options as option}
+        <div class="answers">
+          <label class="radio-button">
+            <input type="radio" name={question.name} value={option.value} />
+            {option.labelText}
+          </label>
+        </div>
+      {/each}
+    </fieldset>
+  {/each}
+
+  <button class="submitButton" type="submit">Submit</button>
+</form>
 
 <style>
   legend {
-    font-size: 40px;
-    font-weight: 700;
+    font-size: 20px;
+    font-weight: bolder;
+    line-height: 2;
     font-family: Figtree;
+    text-align: left;
+    max-width: 534px;
+  }
+
+  input[type="radio"] {
+    display: none;
+  }
+
+  .radio-button {
+    display: inline-block;
+    padding: 1rem 1rem;
+    margin: 0.5rem;
+    border: 2px solid rgba(43, 43, 43, 0.56);
+    border-radius: 12px;
+    background-color: rgba(33, 33, 33, 1);
+    cursor: pointer;
+    transition: all 0.2s ease;
+    font-size: 1rem;
+    user-select: none;
+    text-align: left;
+    line-height: 2;
+  }
+
+  .radio-button input[type="radio"]:checked,
+  .radio-button:has(input[type="radio"]:checked) {
+    border-color: rgb(168, 168, 168);
+    background-color: #d3d7da;
+    color: rgba(33, 33, 33, 1);
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  /* Hover effect */
+  .radio-button:hover {
+    border-color: #999;
+    background-color: rgba(125, 66, 74, 0.7) 100%;
   }
 
   fieldset {
     border: none;
+    text-align: start;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 352px;
+  }
+
+  .answers {
+    @media (min-width: 50rem) {
+      width: 660px;
+    }
+  }
+
+  button {
+    color: white;
+    display: inline-block;
+    padding: 24px;
+    margin: 0.5rem;
+    border: 2px solid rgba(43, 43, 43, 0.56);
+    border-radius: 12px;
+    background-color: rgba(33, 33, 33, 1);
+    cursor: pointer;
+  }
+
+  @media (scripting: enabled) {
+    .submitButton {
+      display: none;
+    }
   }
 </style>
--- src/routes/form-intro/+page.svelte
+++ src/routes/form-intro/+page.svelte
@@ -9,38 +9,20 @@
 
   <QuestionForm {surveyData} />
 
-  <BlurgradPink pageStyle="form-page" />
+  <!-- <BlurgradPink pageStyle="form-page" /> -->
 </main>
 
 <style>
   h1 {
-    font-family: Calvino;
+    font-family: Calvino Grande;
     z-index: 10;
-  }
-  h2,
-  p {
-    font-family: Figtree;
-  }
-  h1 {
     font-size: clamp(2.2rem, 10vw, 3.5rem);
-    font-weight: 600;
-  }
-  h2 {
-    font-weight: 100;
-    font-size: clamp(1.6rem, 5vw, 2rem);
-  }
-
-  p {
-    color: var(--border-grey);
-    font-weight: 100;
-    font-size: clamp(1rem, 5vw, 1.2rem);
-    font-style: italic;
+    font-weight: 800;
   }
 
   main {
     background-color: #1d1d1d;
     align-items: center;
-    background-color: var(--black);
     color: var(--white);
     display: flex;
     flex-direction: column;
@@ -48,49 +30,9 @@
     justify-content: center;
     min-height: 100dvh;
     overflow: hidden;
-    padding: 10em 2em 5em;
+    padding: 2em 2em 5em;
     position: relative;
     text-align: center;
     width: 100%;
-  }
-
-  div:nth-of-type(1),
-  hgroup {
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
-    z-index: 10;
-  }
-
-  div:nth-of-type(1) {
-    gap: 2em;
-  }
-
-  div:nth-of-type(1) h1 {
-    max-width: 15em;
-  }
-
-  progress[value] {
-    appearance: none;
-    border: none;
-    height: 0.25em;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    min-width: 100%;
-  }
-
-  progress[value]::-webkit-progress-bar {
-    background-color: var(--progress-bg);
-    border-radius: 20px;
-  }
-
-  progress[value]::-webkit-progress-value {
-    background-color: var(--white);
-  }
-
-  div:nth-of-type(3) {
-    position: absolute;
-    right: 0em;
-    bottom: 0em;
   }
 </style>
