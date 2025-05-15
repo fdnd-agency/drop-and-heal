498ffd4b5d90ffdbae0c7bd79eb13e8e7b155cf6
Patrickkhr <patrickhrdjk@gmail.com>
2025-05-15 11:45:34 +0200

    feature: #207 added button component styling

--- src/lib/molecules/NavButtons.svelte
+++ src/lib/molecules/NavButtons.svelte
@@ -4,58 +4,88 @@
     export let leftLink = "#"; 
     export let rightLink = "#"; 
     export let borderColor = "#FFFFFF";
+
+    $: layoutClass =
+        leftLink && rightLink
+            ? 'both-buttons'
+            : leftLink
+            ? 'left-only'
+            : rightLink
+            ? 'right-only'
+            : '';
 </script>
 
-<div class="nav-buttons">
-    <a href={leftLink} class="nav-link" style="border: 1px solid {borderColor}">
-        <ArrowL />
-    </a>
-    <a href={rightLink} class="nav-link" style="border: 1px solid {borderColor}">
-        <ArrowR />
-    </a>
+<div class="nav-buttons {layoutClass}">
+    {#if leftLink}
+        <a href={leftLink} class="nav-link">
+            <ArrowL /> <span></span> Vorige
+        </a>
+    {/if}
+    {#if rightLink}
+        <a href={rightLink} class="nav-link">
+            Volgende <span></span> <ArrowR />
+        </a>
+    {/if}
 </div>
 
 <style>
     .nav-buttons {
         display: flex;
         justify-content: space-between;
-        width: 100%;
-        max-width: 7rem;
-        gap: 0.5rem;
-        margin-bottom: 0.5rem; 
-        justify-self: center;
-    }
-
-    .nav-buttons a {
-        background: none;
-        border-radius: 50%;
-        width: 50px;
-        height: 50px;
-        display: flex;
-        justify-content: center;
         align-items: center;
+        width: 100%;
+    }
+    .nav-buttons.left-only {
+        justify-content: flex-start;
+    }
+    .nav-buttons.right-only {
+        justify-content: flex-end;
+    }
+    .nav-buttons.both-buttons {
+        justify-content: space-between;
+        border: 1px solid #2B2B2B;
+        background: #232323;
+        border-radius: 0.75rem;
+        height: 4rem;
+
+        @media (min-width: 1024px) {
+            border: none;
+            background: none;
+            padding: 0;
+        }
+    }
+    .nav-buttons a {
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+        color: inherit;
         cursor: pointer;
-        transition: background-color 0.3s, color 0.3s, border-color 0.3s;
         text-decoration: none;
+        padding: 0.5rem 1rem;
+        span {
+            height: 1.75rem;
+            width: 1px;
+            background-color: #2c2c2c;
+        }
+
+        @media (min-width: 1024px) {
+            padding: 1rem 2rem;
+            background: #232323;
+            border: 1px solid #2B2B2B;
+            border-radius: 0.75rem;
+        }
     }
 
     .nav-buttons a:hover {
         background-color: rgba(255, 255, 255, 0.1); 
-        border-color: rgba(255, 255, 255, 1); 
+        border: 1px  solid rgba(255, 255, 255, 1);
         color: rgba(255, 255, 255, 1); 
+        border-radius: 0.75rem;
+        height: 100%;
     }
 
     .nav-buttons a:disabled {
         opacity: 0.5;
         cursor: not-allowed;
-    }
-
-    .nav-link {
-        color: inherit;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 100%;
     }
 </style>
